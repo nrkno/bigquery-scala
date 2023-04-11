162a25889f3ede84af38bc3a886d2a502f94f40f
Ingar Abrahamsen <ingar.abrahamsen@nrk.no>
2023-04-11 14:45:49 +0200

    feat: support for javasvript UDF
    Support javascript as the language for user defined functions.
    https://cloud.google.com/bigquery/docs/reference/standard-sql/user-defined-functions#javascript-udf-structure

--- core/src/main/scala/no/nrk/bigquery/UDF.scala
+++ core/src/main/scala/no/nrk/bigquery/UDF.scala
@@ -1,20 +1,19 @@
 package no.nrk.bigquery
 
 import no.nrk.bigquery.implicits._
+
 case class UDF(
     name: Ident,
     params: Seq[UDF.Param],
-    body: BQSqlFrag,
+    body: UDF.Body,
     returnType: Option[BQType]
 ) {
-  require(body.asString.startsWith("(") && body.asString.endsWith(")"))
-
   lazy val definition: BQSqlFrag = {
     val returning = returnType match {
       case Some(returnType) => bqfr" RETURNS $returnType"
       case None => BQSqlFrag.Empty
     }
-    bqfr"CREATE TEMP FUNCTION $name${params.map(_.definition).mkFragment("(", ", ", ")")}$returning AS ($body);"
+    bqfr"CREATE TEMP FUNCTION $name${params.map(_.definition).mkFragment("(", ", ", ")")}$returning${body.languageFragment} AS ${body.bodyFragment};"
   }
 
   def apply(args: BQSqlFrag.Magnet*): BQSqlFrag.Call =
@@ -22,6 +21,14 @@ case class UDF(
 }
 
 object UDF {
+  def apply(
+      name: Ident,
+      params: Seq[UDF.Param],
+      body: BQSqlFrag,
+      returnType: Option[BQType]
+  ): UDF =
+    UDF(name, params, UDF.Body.Sql(body), returnType)
+
   case class Param(name: Ident, maybeType: Option[BQType]) {
     def definition: BQSqlFrag =
       maybeType match {
@@ -35,4 +42,36 @@ object UDF {
     def untyped(name: String): Param =
       Param(Ident(name), None)
   }
+
+  sealed trait Body {
+    def bodyFragment: BQSqlFrag
+    def languageFragment: BQSqlFrag
+  }
+  object Body {
+    case class Sql(body: BQSqlFrag) extends Body {
+      require(body.asString.startsWith("(") && body.asString.endsWith(")"))
+      val languageFragment: BQSqlFrag = BQSqlFrag("")
+      override def bodyFragment: BQSqlFrag = bqfr"($body)"
+
+    }
+    case class Js(javascriptSnippet: String, gsLibraryPath: Option[String]) extends Body {
+      val languageFragment: BQSqlFrag = BQSqlFrag(" LANGUAGE js")
+      override def bodyFragment: BQSqlFrag = {
+        val jsBody =
+          bqfr"""|'''
+                 |${BQSqlFrag(javascriptSnippet)}
+                 |'''""".stripMargin
+
+        gsLibraryPath
+          .map(BQSqlFrag.apply)
+          .map(path => bqfr"""OPTIONS ( library="gs://$path" )""") match {
+          case None => jsBody
+          case Some(libraryOption) =>
+            bqfr"""|$jsBody
+                   |$libraryOption""".stripMargin
+        }
+      }
+    }
+  }
+
 }
---
+++ core/src/test/scala/no/nrk/bigquery/UDFTest.scala
@@ -0,0 +1,49 @@
+package no.nrk.bigquery
+
+import munit.FunSuite
+import no.nrk.bigquery.implicits.BQShowInterpolator
+
+class UDFTest extends FunSuite {
+
+  test("render temporary SQL UDF") {
+    assertEquals(
+      UDF(
+        Ident("foo"),
+        Seq(UDF.Param("n", BQType.FLOAT64)),
+        UDF.Body.Sql(bqfr"""(n + 1)"""),
+        Some(BQType.FLOAT64)
+      ).definition.asString,
+      """CREATE TEMP FUNCTION foo(n FLOAT64) RETURNS FLOAT64 AS ((n + 1));"""
+    )
+  }
+
+  test("render temporary javascript UDF") {
+    assertEquals(
+      UDF(
+        Ident("foo"),
+        Seq(UDF.Param("n", BQType.FLOAT64)),
+        UDF.Body.Js("return n + 1", None),
+        Some(BQType.FLOAT64)
+      ).definition.asString,
+      """|CREATE TEMP FUNCTION foo(n FLOAT64) RETURNS FLOAT64 LANGUAGE js AS '''
+         |return n + 1
+         |''';""".stripMargin
+    )
+  }
+
+  test("render temporary javascript UDF with library path") {
+    assertEquals(
+      UDF(
+        Ident("foo"),
+        Seq(UDF.Param("n", BQType.FLOAT64)),
+        UDF.Body.Js("return n + 1", Some("bucket/foo.js")),
+        Some(BQType.FLOAT64)
+      ).definition.asString,
+      """|CREATE TEMP FUNCTION foo(n FLOAT64) RETURNS FLOAT64 LANGUAGE js AS '''
+         |return n + 1
+         |'''
+         |OPTIONS ( library="gs://bucket/foo.js" );""".stripMargin
+    )
+  }
+
+}
