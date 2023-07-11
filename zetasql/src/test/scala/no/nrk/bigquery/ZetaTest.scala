717e0d21d037faa0724e7c63799c526ca9ebe30e
Erlend Hamnaberg <erlend@hamnaberg.net>
2023-07-11 15:25:16 +0200

    select all

--- zetasql/src/test/scala/no/nrk/bigquery/ZetaTest.scala
+++ zetasql/src/test/scala/no/nrk/bigquery/ZetaTest.scala
@@ -1,33 +1,41 @@
 package no.nrk.bigquery
 
-import autovalue.shaded.kotlin.collections.builders.ListBuilder
 import com.google.cloud.bigquery.Field.Mode
 import com.google.cloud.bigquery.StandardSQLTypeName
-import com.google.zetasql.resolvedast.ResolvedNodes
 import no.nrk.bigquery.syntax.bqShowInterpolator
 
 import java.time.LocalDate
 
 class ZetaTest extends munit.FunSuite {
-  test("simple select from example") {
-    val table = BQTableDef.Table(
-      BQTableId(BQDataset.of(ProjectId("com.example"), "example"), "test"),
-      BQSchema.of(
-        BQField("partitionDate", StandardSQLTypeName.DATE, Mode.REQUIRED),
-        BQField("a", StandardSQLTypeName.STRING, Mode.REQUIRED),
-        BQField("b", StandardSQLTypeName.INT64, Mode.REQUIRED),
-        BQField("c", StandardSQLTypeName.INT64, Mode.REQUIRED),
-        BQField("d", StandardSQLTypeName.INT64, Mode.REQUIRED)
-      ),
-      BQPartitionType.DatePartitioned(Ident("partitionDate"))
-    )
+  private val table = BQTableDef.Table(
+    BQTableId(BQDataset.of(ProjectId("com.example"), "example"), "test"),
+    BQSchema.of(
+      BQField("partitionDate", StandardSQLTypeName.DATE, Mode.REQUIRED),
+      BQField("a", StandardSQLTypeName.STRING, Mode.REQUIRED),
+      BQField("b", StandardSQLTypeName.INT64, Mode.REQUIRED),
+      BQField("c", StandardSQLTypeName.INT64, Mode.REQUIRED),
+      BQField("d", StandardSQLTypeName.INT64, Mode.REQUIRED)
+    ),
+    BQPartitionType.DatePartitioned(Ident("partitionDate"))
+  )
 
+  test("subset select from example") {
     val date = LocalDate.of(2023, 1, 1)
 
     val query = bqsql"select partitionDate, a, b, c from ${table.assertPartition(date)}"
 
     val result = ZetaSql.queryFields(query)
     val expected = table.schema.fields.dropRight(1).map(_.recursivelyNullable.withoutDescription)
+    assertEquals(result, expected)
+  }
+
+  test("all fields should be selected from example") {
+    val date = LocalDate.of(2023, 1, 1)
+
+    val query = bqsql"select partitionDate, a, b, c, d from ${table.assertPartition(date)}"
+
+    val result = ZetaSql.queryFields(query)
+    val expected = table.schema.fields.map(_.recursivelyNullable.withoutDescription)
     assertEquals(result, expected)
   }
 }
