9987c73b6137da2f5fa994fdb69a267f744cb4c0
Ingar Abrahamsen <ingar.abrahamsen@nrk.no>
2023-09-11 13:18:14 +0200

    fix: case-sensitive file name issue for scala 3
    trait Nat differs only in case from object nat in package no.nrk.bigquery.internal. Such classes will overwrite one another on case-insensitive filesystems.

---
+++ core/src/main/scala-3/no/nrk/bigquery/internal/NatImpl.scala
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2020 NRK
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package no.nrk.bigquery.internal
+
+sealed trait NatImpl
+type Nat = NatImpl
+
+object nat {
+
+  type _0 = 0 with NatImpl
+  type _1 = 1 with NatImpl
+  type _2 = 2 with NatImpl
+  type _3 = 3 with NatImpl
+  type _4 = 4 with NatImpl
+  type _5 = 5 with NatImpl
+  type _6 = 6 with NatImpl
+  type _7 = 7 with NatImpl
+  type _8 = 8 with NatImpl
+  type _9 = 9 with NatImpl
+  type _10 = 10 with NatImpl
+  type _11 = 11 with NatImpl
+  type _12 = 12 with NatImpl
+  type _13 = 13 with NatImpl
+  type _14 = 14 with NatImpl
+  type _15 = 15 with NatImpl
+  type _16 = 16 with NatImpl
+  type _17 = 17 with NatImpl
+  type _18 = 18 with NatImpl
+  type _19 = 19 with NatImpl
+  type _20 = 20 with NatImpl
+  type _21 = 21 with NatImpl
+  type _22 = 22 with NatImpl
+}
