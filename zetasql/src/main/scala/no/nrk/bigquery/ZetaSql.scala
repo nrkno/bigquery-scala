0359366ca2aed5e72e9f254d72b82fa43f092a97
Erlend Hamnaberg <erlend@hamnaberg.net>
2023-10-10 13:41:48 +0200

    Add space to first fragment
    Removes a BQsqlFrag.Frag allocation

--- zetasql/src/main/scala/no/nrk/bigquery/ZetaSql.scala
+++ zetasql/src/main/scala/no/nrk/bigquery/ZetaSql.scala
@@ -62,7 +62,7 @@ class ZetaSql[F[_]](implicit F: Sync[F]) {
         .distinct
       val (rest, aggregate) = found.foldLeft((0, Vector.empty[BQSqlFrag])) { case ((offset, agg), (t, loc)) =>
         val frag =
-          agg ++ List(BQSqlFrag.Frag(query.substring(offset, loc.start() - 1)), BQSqlFrag.Frag(" "), toFragment(t))
+          agg ++ List(BQSqlFrag.Frag(query.substring(offset, loc.start() - 1) + " "), toFragment(t))
         loc.end() -> frag
       }
       val str = query.substring(rest)
