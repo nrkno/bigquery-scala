1c437f44a57a68c111c586be1b0eb1dfc75c8fae
Erlend Hamnaberg <erlend@hamnaberg.net>
2023-10-16 15:41:02 +0200

    Add codegen for view as well

--- codegen/src/test/scala/no/nrk/bigquery/codegen/CodeGenTest.scala
+++ codegen/src/test/scala/no/nrk/bigquery/codegen/CodeGenTest.scala
@@ -7,28 +7,53 @@
 package no.nrk.bigquery
 package codegen
 
-class CodeGenTest extends munit.FunSuite with testing.GeneratedTest {
+import no.nrk.bigquery.syntax._
 
-  test("generate code") {
+class CodeGenTest extends munit.FunSuite with testing.GeneratedTest {
+  val table = BQTableDef.Table(
+    BQTableId.unsafeFromString("com-example.test.something-important"),
+    BQSchema.of(
+      BQField("partitionDate", BQField.Type.DATE, BQField.Mode.REQUIRED),
+      BQField("name", BQField.Type.STRING, BQField.Mode.REQUIRED),
+      BQField.repeated("list", BQField.Type.INT64),
+      BQField.struct("complex", BQField.Mode.NULLABLE)(
+        BQField("one", BQField.Type.STRING, BQField.Mode.REQUIRED),
+        BQField("two", BQField.Type.STRING, BQField.Mode.REQUIRED),
+        BQField("three", BQField.Type.BOOL, BQField.Mode.NULLABLE)
+      )
+    ),
+    partitionType = BQPartitionType.DatePartitioned(Ident("partitionDate")),
+    description = Some("desc"),
+    clustering = Nil,
+    labels = TableLabels.apply("foo" -> "bar"),
+    TableOptions.Empty
+  )
+
+  test("generate table code") {
+    val generated = CodeGen.generate(
+      List(table),
+      List("example")
+    )
+    val head = generated.head
+    val path = basedir.resolve("codegen-tests")
+    writeAndCompare(head.loc.destinationFile(path), head.asObject)
+  }
+
+  test("generate view code") {
     val generated = CodeGen.generate(
       List(
-        BQTableDef.Table(
-          BQTableId.unsafeFromString("com-example.test.something-important"),
-          BQSchema.of(
-            BQField("partitionDate", BQField.Type.DATE, BQField.Mode.REQUIRED),
-            BQField("name", BQField.Type.STRING, BQField.Mode.REQUIRED),
-            BQField.repeated("list", BQField.Type.INT64),
-            BQField.struct("complex", BQField.Mode.NULLABLE)(
-              BQField("one", BQField.Type.STRING, BQField.Mode.REQUIRED),
-              BQField("two", BQField.Type.STRING, BQField.Mode.REQUIRED),
-              BQField("three", BQField.Type.BOOL, BQField.Mode.NULLABLE)
+        BQTableDef.View(
+          BQTableId.unsafeFromString("com-example.test.something-important-view"),
+          schema = BQSchema
+            .of(
+              table.schema.fields.head,
+              BQField("count", BQField.Type.INT64, BQField.Mode.NULLABLE)
             )
-          ),
+            .recursivelyNullable,
           partitionType = BQPartitionType.DatePartitioned(Ident("partitionDate")),
           description = Some("desc"),
-          clustering = Nil,
-          labels = TableLabels.apply("foo" -> "bar"),
-          TableOptions.Empty
+          query = bqsql"select partitionDate, COUNT(*) from ${table.unpartitioned} group by 1",
+          labels = TableLabels.apply("foo" -> "bar")
         )),
       List("example")
     )
