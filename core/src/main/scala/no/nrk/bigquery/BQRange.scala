af5af6536efb0c20f6485ea3768d3efc1adfa872
Henning Koller <henning.grimeland.koller@nrk.no>
2023-11-30 13:41:46 +0100

    Fix BQRange.calculatePartition

--- core/src/main/scala/no/nrk/bigquery/BQRange.scala
+++ core/src/main/scala/no/nrk/bigquery/BQRange.scala
@@ -10,11 +10,12 @@ import com.google.cloud.bigquery.RangePartitioning
 
 case class BQRange(start: Long, end: Long, interval: Long) {
   def calculatePartition(i: Long): (Long, Long) =
-    (start to end).toList.sliding(interval.toInt, interval.toInt).find(_.contains(i)) match {
-      case Some(head +: _ :+ tail) => (head, tail)
-      case Some(head :: Nil) => (head, head)
-      case None => (start, end)
-    }
+    (start to end).toList
+      .sliding(interval.toInt, interval.toInt)
+      .find(_.contains(i))
+      .collect { case w if w.nonEmpty => (w.head, w.last) }
+      .getOrElse((start, end))
+
 }
 object BQRange {
   def fromRangePartitioning(range: RangePartitioning.Range) =
