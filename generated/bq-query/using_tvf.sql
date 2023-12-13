9c6b7d658e931d1683343e000fc6ac2092eb900b
Ingar Abrahamsen <ingar.abrahamsen@nrk.no>
2023-12-13 15:51:58 +0100

    fix: make it easier to create valid arguments in smoke-test

---
+++ generated/bq-query/using_tvf.sql
@@ -0,0 +1,3 @@
+declare myId default '1';
+select id from `some-project.my-dataset.source-table` where id = myId
+          
---
+++ testing/src/test/scala/no/nrk/bigquery/TvfSmokeTest.scala
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2020 NRK
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+package no.nrk.bigquery
+
+import no.nrk.bigquery.syntax._
+import no.nrk.bigquery.testing.{BQSmokeTest, BigQueryTestClient}
+import no.nrk.bigquery.util._
+
+class TvfSmokeTest extends BQSmokeTest(BigQueryTestClient.testClient) {
+
+  private val schema: BQSchema = BQSchema.of(
+    BQField("id", BQField.Type.STRING, BQField.Mode.NULLABLE)
+  )
+  private val myDataset: BQDataset = BQDataset(ProjectId.unsafeFromString("some-project"), "my-dataset", None)
+  private val sourceTable = BQTableDef.Table(
+    BQTableId.unsafeOf(myDataset, "source-table"),
+    schema,
+    BQPartitionType.NotPartitioned
+  )
+  val tvf: TVF[Unit, nat._1] = TVF(
+    TVF.TVFId(myDataset, ident"my_tvf"),
+    BQPartitionType.NotPartitioned,
+    BQRoutine.Params(BQRoutine.Param("myId", BQType.STRING)),
+    bqfr"select id from $sourceTable where id = myId",
+    schema,
+    None
+  )
+
+  bqCheckTableValueFunction("using_tvf", tvf)(args => args(StringValue("1")))
+
+}
