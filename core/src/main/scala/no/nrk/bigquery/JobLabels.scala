465e837c8bc18ed1525d40eefec59c1814f615af
Erlend Hamnaberg <erlend@hamnaberg.net>
2024-01-08 16:16:16 +0100

    Add Key and Value data types to make construction easier

--- core/src/main/scala/no/nrk/bigquery/JobLabels.scala
+++ core/src/main/scala/no/nrk/bigquery/JobLabels.scala
@@ -22,33 +22,13 @@ object JobLabels {
 
   val empty = JobLabels(SortedMap.empty[String, String])
 
-  def validated(values: SortedMap[String, String]): Either[NonEmptyChain[String], JobLabels] = {
-    def check(key: String)(op: String => Boolean, msg: String) =
-      if (op(key)) msg.invalidNec else key.validNec
-
-    def checkLength(v: String, mode: String) = check(v)(_.length > 63, show"label $mode $v is longer than 63 chars")
-    def checkChars(v: String, mode: String) = check(v)(
-      !_.forall(c => c.isLower || c.isDigit || c == '-' || c == '_'),
-      show"label $mode $v can contain only lowercase letters, numeric characters, underscores, and dashes"
-    )
-
+  def validated(values: SortedMap[String, String]): Either[NonEmptyChain[String], JobLabels] =
     values.toList
       .traverse { case (key, value) =>
-        val checkKey =
-          (
-            check(key)(_.isEmpty, show"label key $key is empty"),
-            checkLength(key, "key"),
-            checkChars(key, "key"),
-            check(key)(!_(0).isLower, show"label key $key must start with lowercase letter or international character"))
-            .mapN((_, _, _, _) => key)
-
-        val checkValue = (checkLength(value, "value"), checkChars(value, "value")).mapN((_, _) => value)
-
-        (checkKey, checkValue).tupled
+        (Key.apply(key), Value.apply(value)).tupled
       }
-      .map(list => JobLabels(SortedMap(list: _*)))
+      .map(list => JobLabels.from(list))
       .toEither
-  }
 
   /** truncates string to 63 chars. Replaces all non-word chars with _ Users should make sure this cannot result in an
     * empty string, as that will fail validation,
@@ -61,10 +41,66 @@ object JobLabels {
   def safeString(value: String): String =
     value.trim.substring(0, Math.min(63, value.length)).toLowerCase().replaceAll("\\W", "_")
 
-  def apply(params: (String, String)*): Either[NonEmptyChain[String], JobLabels] = validated(SortedMap(params: _*))
+  def from(params: scala.collection.immutable.Seq[(Key, Value)]): JobLabels = JobLabels(SortedMap(params.map {
+    case (k, v) =>
+      k.value -> v.value
+  }: _*))
+
+  def apply(params: (Key, Value)*): JobLabels = from(params)
 
   def unsafeFrom(params: (String, String)*): JobLabels =
     validated(SortedMap(params: _*))
       .fold(messages => throw new IllegalArgumentException(messages.toList.mkString("\n")), identity)
 
+  final class Key private (val value: String) {
+    override def hashCode(): Int = 31 * value.hashCode
+
+    override def equals(obj: Any): Boolean = obj match {
+      case k: Key => value == k.value
+      case _ => false
+    }
+
+    override def toString: String = value
+  }
+  object Key {
+    def apply(key: String) =
+      (
+        check(key)(_.isEmpty, show"label key $key is empty"),
+        checkLength(key, "key"),
+        checkChars(key, "key"),
+        check(key)(!_(0).isLower, show"label key $key must start with lowercase letter or international character"))
+        .mapN((_, _, _, _) => new Key(key))
+
+    def unsafeFromString(value: String): Key =
+      apply(value).fold(messages => throw new IllegalArgumentException(messages.toList.mkString("\n")), identity)
+  }
+
+  final class Value private (val value: String) {
+    override def hashCode(): Int = 31 * value.hashCode
+
+    override def equals(obj: Any): Boolean = obj match {
+      case k: Value => value == k.value
+      case _ => false
+    }
+
+    override def toString: String = value
+  }
+
+  object Value {
+    def apply(value: String) =
+      (checkLength(value, "value"), checkChars(value, "value")).mapN((_, _) => new Value(value))
+
+    def unsafeFromString(value: String): Value =
+      apply(value).fold(messages => throw new IllegalArgumentException(messages.toList.mkString("\n")), identity)
+  }
+
+  private def check(key: String)(op: String => Boolean, msg: String) =
+    if (op(key)) msg.invalidNec else key.validNec
+
+  private def checkLength(v: String, mode: String) =
+    check(v)(_.length > 63, show"label $mode $v is longer than 63 chars")
+  private def checkChars(v: String, mode: String) = check(v)(
+    !_.forall(c => c.isLower || c.isDigit || c == '-' || c == '_'),
+    show"label $mode $v can contain only lowercase letters, numeric characters, underscores, and dashes"
+  )
 }
