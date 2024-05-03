287fe7eedd759cc4ccc48680bfb058da3aa3191b
Tor Fredrik Sommerdal <tor.fredrik.sommerdal@nrk.no>
2024-05-03 12:23:08 +0200

    change other type too

--- testing/src/test/scala/no/nrk/bigquery/ProductBQReadTest.scala
+++ testing/src/test/scala/no/nrk/bigquery/ProductBQReadTest.scala
@@ -16,15 +16,15 @@ class ProductBQReadTest extends BQSmokeTest(Http4sTestClient.testClient) {
     implicit val bqRead: BQRead[SubStructure] = BQRead.derived
   }
 
-  case class TestCase(num: String, subs: List[SubStructure])
+  case class ComplexType(num: String, subs: List[SubStructure])
 
-  object TestCase {
-    implicit val bqRead: BQRead[TestCase] =
-      BQRead.forProduct2("numberOfTests", "tests")((n, tests) => TestCase(n, tests))
+  object ComplexType {
+    implicit val bqRead: BQRead[ComplexType] =
+      BQRead.forProduct2("numberOfTests", "tests")((n, tests) => ComplexType(n, tests))
   }
 
   bqTypeCheckTest("nested structure") {
-    BQQuery[TestCase](
+    BQQuery[ComplexType](
       bqsql"""SELECT * FROM UNNEST(ARRAY<STRUCT<numberOfTests STRING, tests ARRAY<STRUCT<a STRING, b INT64>>>>[("a", [("b", 10), ("c", 11)])])"""
     )
   }
