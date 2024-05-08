126495d44b2589333946e091dbd83377c3748e46
Erlend Hamnaberg <erlend@hamnaberg.net>
2024-05-08 15:53:13 +0200

    This is apparently not a warning now

--- testing/src/main/scala/no/nrk/bigquery/testing/package.scala
+++ testing/src/main/scala/no/nrk/bigquery/testing/package.scala
@@ -10,8 +10,6 @@ import cats.syntax.show.*
 import io.circe.{Decoder, Encoder}
 import no.nrk.bigquery
 
-import scala.annotation.nowarn
-
 package object testing {
   def tempTable(partitionId: BQPartitionId[Any]): Ident =
     Ident(partitionId.show.filter(c => c.isLetterOrDigit || c == '_'))
@@ -19,18 +17,18 @@ package object testing {
   implicit val encoderIdent: Encoder[Ident] = Encoder.encodeString.contramap(_.value)
 
   implicit lazy val encodeField: Encoder[BQField] = {
-    @nowarn implicit val encodesStandardSQLTypeName: Encoder[BQField.Type] =
+    implicit val encodesStandardSQLTypeName: Encoder[BQField.Type] =
       Encoder[String].contramap(_.name)
 
-    @nowarn implicit val encodesFieldMode: Encoder[bigquery.BQField.Mode] =
+    implicit val encodesFieldMode: Encoder[bigquery.BQField.Mode] =
       Encoder[String].contramap(_.name)
     io.circe.generic.semiauto.deriveEncoder
   }
 
   implicit lazy val decodeField: Decoder[BQField] = {
-    @nowarn implicit val decodesStandardSQLTypeName: Decoder[BQField.Type] =
+    implicit val decodesStandardSQLTypeName: Decoder[BQField.Type] =
       Decoder[String].map(BQField.Type.unsafeFromString)
-    @nowarn implicit val decodesFieldMode: Decoder[BQField.Mode] =
+    implicit val decodesFieldMode: Decoder[BQField.Mode] =
       Decoder[String].map(BQField.Mode.unsafeFromString)
     io.circe.generic.semiauto.deriveDecoder
   }
