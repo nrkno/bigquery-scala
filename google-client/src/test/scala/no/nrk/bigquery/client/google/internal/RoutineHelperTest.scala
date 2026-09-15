a63511138da2e69a652c43e1aa86b67edd3e4737
Eric Törn <eric.toern@nrk.no>
2026-09-15 10:34:30 +0200

    fix test

--- google-client/src/test/scala/no/nrk/bigquery/client/google/internal/RoutineHelperTest.scala
+++ google-client/src/test/scala/no/nrk/bigquery/client/google/internal/RoutineHelperTest.scala
@@ -76,7 +76,7 @@ class RoutineUpdateOperationTest extends FunSuite {
       .newBuilder(routineId)
       .setRoutineType("SCALAR_FUNCTION")
       .setLanguage("SQL")
-      .setBody("((1))")
+      .setBody("(1)")
       .setReturnType(StandardSQLDataType.newBuilder().setTypeKind(BQType.INT64.tpe.name).build())
       .build()
 
